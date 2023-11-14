121491a33cc6485cca13f3e5c3a042eaf7ea0a92
ccn <ccn@cuppajoeman.com>
2023-11-14 14:52:34 -0500

    add many new blog posts, toolbox

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,16 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+  // eleventyConfig.addPassthroughCopy("toolbox");
+
+  // Copy any .jpg/png file to `_site`, via Glob pattern
+  // Keeps the same directory structure.
+  eleventyConfig.addPassthroughCopy("**/*.jpg");
+  eleventyConfig.addPassthroughCopy("**/*.png");
+
+  // coding files
+  eleventyConfig.addPassthroughCopy("**/*.py");
+  eleventyConfig.addPassthroughCopy("**/*.sh");
+  eleventyConfig.addPassthroughCopy("**/*.js");
+  eleventyConfig.addPassthroughCopy("**/*.vim");
 };
